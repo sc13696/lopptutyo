ce9b4c3543ad0f4e983f12b1d63e95c60225f8fb
sc13696 <sc13696@student.samk.f>
2025-12-01 09:54:02 +0200

    arrow keys support
    reimplemented imput bytes handler, to:
    - support multibyte keys
    - suport arrow keys
    - dedicated handler for keys

--- components/input.py
+++ components/input.py
@@ -24,3 +24,53 @@ except ImportError:
         if msvcrt.kbhit():
             return msvcrt.getch()
         return None
+
+
+
+
+
+
+import sys, os
+
+# ---------- Windows ----------
+try:
+    import msvcrt
+
+    def get_key():
+        if not msvcrt.kbhit():
+            return None
+
+        c = msvcrt.getch()
+
+        if c in (b'\x00', b'\xe0'):
+            return c + msvcrt.getch()     # arrow, f-keys
+
+        return c                          # normal byte
+
+# ---------- Linux / POSIX ----------
+except ImportError:
+    import termios, tty, select
+
+    def get_key():
+        dr,_,_ = select.select([sys.stdin], [], [], 0)
+        if not dr:
+            return None
+
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            c = os.read(fd, 1)
+
+            if c == b'\x1b':              # possible arrow sequence
+                seq = c
+                while True:
+                    r,_,_ = select.select([sys.stdin], [], [], 0)
+                    if not r:
+                        break
+                    seq += os.read(fd, 1)
+                return seq                # e.g. b'\x1b[A'
+            return c
+
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
